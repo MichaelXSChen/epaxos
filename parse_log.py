2d561b6d614e40ae9170014466278d50e89dab99
mxsc <michael.xschen@gmail.com>
2019-08-08 15:54:48 +0800

    asymetric TC done

--- parse_log.py
+++ parse_log.py
@@ -24,15 +24,21 @@ for f in files:
 
     commit_mean = df['commit'].mean()
     commit_max = df['commit'].max()
+    commit_std = df['commit'].std()
 
 
     delay_mean = df['delay'].mean()
     delay_max = df['delay'].max()
+    delay_std = df['delay'].std()
 
     total_mean = df['total'].mean()
     total_max = df['total'].max()
+    total_std = df['total'].std()
+
+
 
     print('\n\n%s' % f)
     print('      commmit   delay   total')
     print('mean  %0.2fms  %0.2fms  %0.2fms'% (commit_mean/1000, delay_mean/1000, total_mean/1000))
     print('max   %0.2fms  %0.2fms  %0.2fms'% (commit_max/1000, delay_max/1000, total_max/1000))
+    print('std   %0.2f    %0.2f    %0.2f  '% (commit_std, delay_std, total_std))
